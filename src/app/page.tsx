08a09c255786f41cf2973cf3d21fd1ce4efbb530
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-02-23 22:35:18 +0530

    CHORE: NextJS and shadcn setup

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <>
+            Hello World
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. A dolore fuga quisquam atque impedit. Tenetur impedit facere illum suscipit similique vel voluptatum voluptatem eveniet? Voluptas, unde? Corrupti corporis odio iure.
+        </>
+    );
+}
