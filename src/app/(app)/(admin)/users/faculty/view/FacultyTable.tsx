896df7182b3dd21343d28003355f386a5383b8b9
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-11-08 14:05:18 +0530

    FEAT: Added Faculty Update, Delete functions for admin

--- src/app/(app)/(admin)/users/faculty/view/FacultyTable.tsx
+++ src/app/(app)/(admin)/users/faculty/view/FacultyTable.tsx
@@ -7,6 +7,17 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 import { ArrowUpDownIcon, MoreHorizontal, PencilIcon, Trash2Icon } from "lucide-react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { IFacultyView } from "@/types/faculty";
+import { AlertDialog, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { Form, FormField, FormItem, FormLabel, FormMessage, FormControl } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Loader2Icon } from "lucide-react";
+import { useState } from "react";
+import { useToast } from "@/lib/hooks/use-toast";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { updateFacultySchema } from "@/schemas/faculty";
+import { z } from "zod";
+import { deleteFaculty, updateFaculty } from "@/actions/admin/faculties";
 
 const columns: ColumnDef<IFacultyView>[] = [
     {
@@ -61,25 +72,8 @@ const columns: ColumnDef<IFacultyView>[] = [
     },
     {
         id: "actions",
-        cell: ({ }) => {
-            return (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 p-0 focus:outline-none focus-visible:outline-none focus:border-collapse focus-visible:border-collapse focus-visible:border-none">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal className="h-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <DropdownMenuItem className="flex items-center gap-2">
-                            <PencilIcon className="w-4 h-4" /> Edit
-                        </DropdownMenuItem>
-                        <DropdownMenuItem className="flex items-center gap-2">
-                            <Trash2Icon className="w-4 h-4 text-red-600" /> Delete
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            );
+        cell: ({ row }) => {
+            return <Actions row={row} />;
         },
     },
 ];
@@ -89,5 +83,186 @@ export const FacultyTable = ({ faculties }: { faculties: IFacultyView[] }) => {
         <>
             <DataTable columns={columns} data={faculties} />
         </>
+    );
+};
+
+const Actions = ({ row }: { row: any }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isEditOpen, setIsEditOpen] = useState(false);
+    const faculty = row.original;
+    const [isLoading, setIsLoading] = useState(false);
+    const { toast } = useToast();
+    const form = useForm<z.infer<typeof updateFacultySchema>>({
+        resolver: zodResolver(updateFacultySchema),
+        defaultValues: {
+            name: faculty.name,
+            email: faculty.email,
+        }
+    });
+    async function onSubmit(values: z.infer<typeof updateFacultySchema>) {
+        setIsLoading(true);
+        let res = await updateFaculty({
+            facultyId: faculty._id,
+            faculty: {
+                name: values.name,
+                email: values.email,
+            }
+        });
+        setIsLoading(false);
+        setIsOpen(false);
+        form.resetField("name");
+        form.resetField("email");
+        form.reset();
+        if (res.status === 200) {
+            toast({
+                title: "Faculty Updated 🎉",
+                variant: "success",
+                description: values.name + " has been updated in the database",
+            });
+        } else {
+            toast({
+                title: "Failed to update faculty 😞",
+                variant: "destructive",
+                description: JSON.parse(JSON.stringify(res.message)),
+            });
+        }
+    }
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 p-0 focus:outline-none focus-visible:outline-none focus:border-collapse focus-visible:border-collapse focus-visible:border-none">
+                    <span className="sr-only">Open menu</span>
+                    <MoreHorizontal className="h-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <AlertDialog open={isOpen} onOpenChange={setIsOpen} defaultOpen={false}>
+                    <AlertDialogTrigger asChild onClick={(e) => {
+                        e.preventDefault();
+                        setIsOpen(true);
+                    }}>
+                        <DropdownMenuItem className="flex items-center gap-2">
+                            <PencilIcon className="w-4 h-4 text-indigo-600" /> Edit
+                        </DropdownMenuItem>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent className="sm:max-w-[425px]">
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Edit Student</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                Edit the details of the student.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                                <FormField
+                                    control={form.control}
+                                    name="name"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Name</FormLabel>
+                                            <FormControl>
+                                                <Input type="text"
+                                                    placeholder="Enter Student Name"
+                                                    {...field}
+                                                    defaultValue={faculty.name}
+                                                    value={form.watch("name") || faculty.name}
+                                                    onChange={(e: any) => {
+                                                        form.setValue("name", e.target.value);
+                                                        form.setValue("email", e.target.value.length > 0 ? e.target.value.toLowerCase().replace(/\s/g, "") + "@faculty.cs.com" : "");
+                                                    }}
+                                                />
+                                            </FormControl>
+                                            <FormMessage className="text-sm" />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem
+                                            className="opacity-70 cursor-not-allowed"
+                                            title="Email is auto generated based on the name"
+                                        >
+                                            <FormLabel>Email</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    placeholder="Faculty Email"
+                                                    type="email"
+                                                    disabled={true}
+                                                    defaultValue={form.watch("email") || faculty.email}
+                                                    value={form.watch("email") || faculty.email}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                            <FormMessage className="text-sm" />
+                                        </FormItem>
+                                    )}
+                                />
+                                <AlertDialogFooter>
+                                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                    <Button variant="primary"
+                                        size={"default"}
+                                        type="submit"
+                                        disabled={isLoading}
+                                    >
+                                        {isLoading ?
+                                            <Loader2Icon className="animate-spin" size={18} />
+                                            : "Save changes"}
+                                    </Button>
+                                </AlertDialogFooter>
+                            </form>
+                        </Form>
+                    </AlertDialogContent>
+                </AlertDialog>
+                <AlertDialog open={isEditOpen} onOpenChange={setIsEditOpen} defaultOpen={false}>
+                    <AlertDialogTrigger asChild onClick={(e) => {
+                        e.preventDefault();
+                        setIsEditOpen(true);
+                    }}>
+                        <DropdownMenuItem className="flex items-center gap-2">
+                            <Trash2Icon className="w-4 h-4 text-red-600" /> Delete
+                        </DropdownMenuItem>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Delete Faculty</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                Are you sure you want to delete <b>{faculty.name}</b> with email <b>{faculty.email}</b>?
+                                <br />
+                                <span className="text-red-600">Note: This action cannot be undone.</span>
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <Button
+                                onClick={async () => {
+                                    setIsEditOpen(false);
+                                    let res = await deleteFaculty({
+                                        facultyId: faculty._id
+                                    });
+                                    if (res.status === 200) {
+                                        toast({
+                                            title: "Faculty Deleted 🎉",
+                                            variant: "success",
+                                            description: faculty.name + " has been deleted from the database",
+                                        });
+                                    } else {
+                                        toast({
+                                            title: "Failed to delete faculty 😞",
+                                            variant: "destructive",
+                                            description: JSON.parse(JSON.stringify(res.message)),
+                                        });
+                                    }
+                                }}
+                                variant="destructive"                                    >
+                                Delete
+                            </Button>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
+            </DropdownMenuContent>
+        </DropdownMenu>
     );
 };
--- src/schemas/faculty.ts
+++ src/schemas/faculty.ts
@@ -18,3 +18,22 @@ export const createFacultySchema = z.object({
             message: "Invalid Email",
         }),
 });
+
+export const updateFacultySchema = z.object({
+    name: z
+        .string({
+            required_error: "Name is required",
+        }).min(3, {
+            message: "Name should be atleast 3 characters long",
+        }).max(50, {
+            message: "Name should be atmost 50 characters long",
+        }).regex(/^[a-zA-Z ]+$/, {
+            message: "Name should contain only alphabets",
+        }),
+    email: z
+        .string({
+            required_error: "Email is required",
+        }).email({
+            message: "Invalid Email",
+        }),
+});
